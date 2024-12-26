afe60e1c469ac27284d067106c71aec15907d693
amamtaiwan <43307772+amamtaiwan@users.noreply.github.com>
2024-12-26 21:57:45 +0800

    Add files via upload

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,7 @@
-gdown --folder "https://drive.google.com/drive/folders/1ML_qygqmzk43WZ_IepYmwqojhm1V1HKW?usp=drive_link" -O Data
-gdown --folder "https://drive.google.com/drive/folders/1yO45bGoNmDAMh2eQRCoLBEa5ECsUABJJ?usp=drive_link" -O Data_mask_sam
-gdown --folder "https://drive.google.com/drive/folders/1HuYpaivrFZqhD5YQNZTZ82uxu29r5IPa?usp=drive_link" -O inf
+gdown "https://drive.google.com/uc?id=1qCpJkh5JyvDNwX8D-Kv-owjQ3v5N6CcL" -O Data.tar
+gdown "https://drive.google.com/uc?id=1VWX6x2JIaS968XQK141id8AuTSrxetXB" -O Data_mask_sam.tar
+gdown "https://drive.google.com/uc?id=1WNzDrxZas-neoHM8EmmHUsLdU5vWXDBx" -O inf.tar
+
+tar xvf Data.tar
+tar xvf Data_mask_sam.tar
+tar xvf inf.tar
